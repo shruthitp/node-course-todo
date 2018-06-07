8df5071fa58f1e28c387c8821e1f32d3397bdfec
shruthi T P <shruthtp@in.ibm.com>
2018-06-07 15:33:53 +0530

    Add tests for GET /todos

--- server/server.js
+++ server/server.js
@@ -24,16 +24,16 @@ app.post('/todos', (req, res) => {
 });
 
 
-app.get('/todos', (req , res)=>{
+app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
-        res.send({todos});
-    }, (err)=>{
+        res.send({ todos });
+    }, (err) => {
         res.status(400).send(e);
-    })
+    });
 });
 
 app.listen(3000, () => {
     console.log('Started at port 3000');
 });
 
-module.exports = {app};
+module.exports = { app };
